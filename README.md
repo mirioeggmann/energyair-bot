5be0356092a1ad05010d996aedc11876a9ecde7b
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2017-08-20 21:57:58 +0200

    Adapted for new version

--- README.md
+++ README.md
@@ -1,35 +1,39 @@
 # energyair-bot
 The energyair-bot should only be used for educational purposes. Otherwise it is unfair for the other persons that play the game for real. You can try the bot **but at your own risk!**
 
-Work is still in progress and I just tried to get into selenium browser automation. (v0.5)!
-
 ## Getting started
-### Infos
-- When you stop the script before you collected your win the browser window will freeze and you won't be able to do it anymore. (So enter the phone number while the script is still running)
-- The python script generally runs until you stop it. However sometimes there are unexpected errors and so it is useful to run a bash / powershell script that calls the python script.
 
 ### Requirements
-- [Python 3.3+](https://www.python.org/downloads/)
-- [Python bindings for Selenium](https://pypi.python.org/pypi/selenium)
-- git
+- [Python 3.6](https://www.python.org/downloads/release/python-360/)
+- [Git+Git Bash](https://git-scm.com/downloads)
 
 ### Setup
 
-#### Ubuntu / Debian
-1. Clone the repo: `git clone https://github.com/luvirx/energyair-bot`
-2. Navigate to the new folder and run the bash script
+#### Windows
+1. Download Python 3.6 and add it to the PATH
+2. Download Git and Git Bash
+3. Open Git Bash and enter the following commands:
 ```bash
-cd PATH_TO_YOUR_THE_NEW_FOLDER
-./race.sh
+py -m pip install lxml
+py -m pip install requests
+git clone https://github.com/mirioeggmann/energyair-bot.git
+cd energyair-bot
+```
+4. Search the downloaded folder "energyair-bot" and open the energyair-bot.py file with a text editor
+5. **Replace the phoneNumber = '0791112233' with your own phone number**
+6. Return to the Git Bash and enter the following command:
+```bash
+phoneNumber = '0791112233'
 ```
 
-#### Windows
+#### Ubuntu / Debian
+
+
+#### OSX
+
 
 ## Issues
 [GitHub Issues](https://github.com/luvirx/energyair-bot/issues) - Got issues? Please tell me, but first check if someone already created one for your problem.
-
-## Features
-- [ ] Submitting the tickets directly to a custom phone number.
 
 ## License
 Licensed under the [MIT license](https://opensource.org/licenses/MIT)
--- energyair-bot.py
+++ energyair-bot.py
@@ -1,80 +1,157 @@
-# coding=utf-8
-
 # Script:  energyair-bot.py
-# Version: v0.5
-# Author:  luvirx
-# Run:     ./race.sh
+# Version: v2.0
 
 # Imports
-from selenium import webdriver
-import time
+import sys
+from lxml import html
+import requests
+
+unanswered = dict()
+errors = 0
+wrong = 0
+rounds = 0
+schief = 0
+phoneNumber = '0791112233'
 
 # Questions and answers
 questions = {
-    'Wo findet das grösste Jazz-Festival Europas statt?' : 2,
-    'Woher stammt Kungs?' : 1,
-    'Mit welchem Hit gelang Kungs der europaweite Durchbruch?' : 1,
-    'Welche Sängerin singt im Remix «Dont Be So Shy» von Filatov & Karas?' : 3,
-    'Die wievielte Ausgabe des Energy Air findet 2016 statt?' : 2,
-    'Wie heisst Dua Lipas neuster Hit?' : 2,
-    'Was gehört für viele bei einem Festival auf den Kopf?' : 3,
-    'Wie war der Slogan des legendären Woodstock Festival?' : 1,
-    'Als was arbeitete Dua Lipa vor ihrer Musikkarriere?' : 2,
-    'Woher stammen Dua Lipas Eltern?' : 3,
-    'Welcher Energy Air Act trat schon mal am Tomorrowland auf?' : 3,
-    'Welches Festival findet in der gleichen Stadt statt wie Energy Air?' : 2,
-    'Wie heisst Manillios aktuelles Album?' : 1,
-    'Wie heisst der neue Song von Kungs?' : 2,
-    'Wie heisst der Frontsänger von OneRepublic?' : 3,
-    'In welcher Stadt findet das Energy Air 2016 statt?' : 1,
-    'Woher stammt das DJ-Duo Filatov & Karas?' : 2,
-    'Was ist ein Line-Up?' : 2,
-    'In welchem Kanton findet das Gampel Openair statt?' : 3,
-    'Wie lautet der Slogan des Energy Air?' : 3,
-    'Welche Acts kommen ans Energy Air?' : 1,
-    'Wo findet das Energy Air dieses Jahr statt?' : 1,
-    'Welche Energy Radiostation existiert nicht?' : 3,
-    'In welchem Jahr wurde Kungs geboren?' : 2,
-    'Wie lautet der offizielle Hashtag für das Energy Air 2016?' : 1,
-    'Woher kommt das «Holi Festival of Colours» ursprünglich?' : 2,
-    'Welches Sujet taucht rund ums Energy Air immer wieder auf?' : 1,
-    'Wer stand letztes Jahr bei Energy Air als erstes auf der Bühne?' : 3,
-    'Wie heisst der offizielle Energy Air Song 2015?' : 3,
-    'Wie viel Technikmaterial wurde letztes Jahr für die Show benötigt?' : 3,
-    'Wo ist Rapper Manillio aufgewachsen?' : 3,
-    'Wie lautet Manillios bürgerlicher Name?' : 1,
-    'Aus wie vielen Musikern besteht OneRepublic?' : 2,
-    'In welchem Bundesstaat feiern Musikbegeisterte das Coachella Festival?' : 2,
-    'Wie viele Konzertliebhaber feiern das Energy Air jedes Jahr?' : 3,
-    'Welcher dieser Acts war am letzten Energy Air nicht dabei?' : 3
+    'Wann fand das erste Energy Air statt?' : 1,
+    'Wann ist die offizielle Türöffnung beim Energy Air?' : 1,
+    'Von wem wird das Energy Air durchgeführt?' : 1,
+    'Wer hatte den letzten Auftritt am Energy Air 2016?' : 1,
+    'Welche Plätze gibt es am Energy Air?' : 3,
+    'Wie heissen zwei andere grosse Events von Energy?' : 1,
+    'Wie hiess die Energy Air Hymne 2015?' : 1,
+    'Welcher Künstler stand NOCH NIE auf der Energy Air Bühne?' : 1,
+    'Ab wann darf man, ohne eine erwachsene Begleitperson, am Energy Air teilnehmen?' : 3,
+    'Welcher dieser Acts Stand schon auf der Stade de Suisse Bühne?' : 2,
+    'Von welchem ehemaligen Energy Air Act ist der Song «Bilder im Kopf»?' : 1,
+    'Welcher DJ stand noch nie auf der Energy Air Bühne?' : 2,
+    'Wann fand das Energy Air letztes Jahr statt?' : 1,
+    'Welcher Energy Air Act aus den letzten Jahren stand nur mit seinem Gitarristen auf der Bühne?' : 1,
+    'Welches Stadion ist das grösste der Schweiz?' : 1,
+    'Wie oft pro Jahr findet das Energy Air statt?' : 3,
+    'Was bedeutet Air auf Deutsch?' : 2,
+    'Wann wurde das Stade de Suisse offiziell fertig gestellt?' : 1,
+    'Welcher Pop-Sänger stand in diesem Jahr schon auf der Bühne des Stade de Suisse?' : 1,
+    'Welche deutsche Sängerin stand letztes Jahr auf der Energy Air Bühne?' : 1,
+    'Mit welchem Künstler stand Schlangenfrau Nina Burri auf der Bühne?' : 1,
+    'In welchem Jahr stand OneRepublic auf der Bühne des Energy Air?' : 1,
+    'Welcher Act performte in einem Karton-Hippie-Bus?' : 3,
+    'Wie heissen die beiden anderen grossen Events von Energy?' : 1,
+    'Wann findet das diesjährige Energy Air statt?' : 2,
+    'Was ist das Energy Air als einziger Energy Event?' : 1,
+    'Wie lautet der offizielle Energy Air Hashtag?' : 3,
+    'Wo findet das Energy Air statt?' : 3,
+    'Wann ist offiziell Türöffnung beim Energy Air?' : 1,
+    'Wie viele Sitzplätze hat das Stade de Suisse bei Sport Veranstaltungen?' : 1,
+    'Wo kann man Energy Air Tickets kaufen?' : 3,
+    'Zum wievielten Mal findet das Energy Air statt?' : 2,
+    'Von welchem ehemaligen Energy Air Act ist der Song «Bilder im Kopf»?' : 1,
+    'Welcher Act stand NOCH NIE auf der Energy Air Bühne?' : 3,
+    'Wie hiess das Stade de Suisse früher?' : 1,
+    'Wie viel kostet ein Energy Air Ticket?' : 1,
+    'Welcher Act stand NOCH NIE auf der Energy Air Bühne?' : 1,
+    'Welche Farben dominieren das Energy Air Logo?' : 3,
+    'Welcher Act stand schon einmal auf der Energy Air Bühne?' : 1,
+    'Wer war der letzte Act am Energy Air 2016?' : 3,
+    'In welchen Schweizer Stadt hat Energy KEIN Radiostudio?' : 3,
+    'Wann fand das erste Energy Air statt?' : 1,
+    'Von welchem vergangenen Energy Air Act ist der Song «Angelina»?' : 1,
+    'Wie viele Zuschauer passen ins Stade de Suisse?' : 1,
+    'Wie hiess der Energy Air Song im Jahr 2014?' : 3,
+    'Wie viele Tage dauert das Energy Air?' : 3,
+    'Von wem wird das Energy Air durchgeführt?' : 1,
+    'Wie viel kostet die Energy Air App?' : 2,
+    'Wie viele Tickets werden für das Energy Air verlost?' : 1,
+    'Wie gross ist die Spielfläche des Stade de Suisse?' : 2,
+    'Wie hiess im Jahr 2015 die Energy Air Hymne?' : 1,
+    'Das Energy Air ist ...?' : 1,
+    'Wie viele Male standen Dabu Fantastic bereits auf der Energy Air Bühne?' : 1,
+    'Welcher Fussballverein ist im Stade de Suisse Zuhause?' : 1,
+    'Ab wann darf man am Energy Air teilnehmen?' : 1,
+    'Was ist die obere Altersbeschränkung des Energy Air?' : 2,
+    'In welchem Monat findet das Energy Air jeweils statt?' : 2,
+    'Was für Plätze gibt es am Energy Air?' : 3,
+    'In welcher Schweizer Stadt hat Energy KEIN Radiostudio?' : 3
 }
 
-# Start Firefox and open the game site
-driver = webdriver.Firefox()
-driver.get('https://game.energy.ch')
 
-# Start the game
-driver.find_element_by_link_text('Los gehts!').click()
+def get_answer(question):
+    print(question)
+    answer = questions.get(question, 0)
+    if answer == 0:
+        print("unanswered")
+        unanswered.update({question: 0})
+        return 1
+    return answer
 
-# Answer all questions with the help of the answer list
-for i in range(10):
-    question = driver.find_element_by_tag_name('h1').text
-    answer = questions[question.encode('utf-8', 'ignore')]
-    driver.find_elements_by_tag_name('label')[answer-1].click()
-    driver.find_element_by_id('submitQuestion').click()
-    time.sleep(1)
 
-# If all answers were correct
-if(driver.find_element_by_tag_name('h1').text.encode('utf-8','ignore') == "Glückwunsch!"):
-    
-    # Click on 1 of the 12 logos
-    time.sleep(3)
-    driver.find_element_by_tag_name('button').click()
-    time.sleep(3)
-    driver.find_elements_by_tag_name('a')[6].click()
+def next_question(antwort):
+    data = {'question': antwort}
+    q2 = session.post('https://game.energy.ch/', data)
+    tree = html.fromstring(q2.content)
+    frage = tree.xpath('//form[@class="question"]/h1/text()')[0]
+    return frage
 
-    # Close the window if you didn't win
-    if(driver.find_element_by_tag_name('h1').text == "Das war das falsche Logo, knapp daneben! Versuche es erneut!"):
-        driver.quit()
-else:
-    driver.quit()
+
+try:
+    while True:
+        try:
+            rounds += 1
+            session = requests.session()
+            data = {'mobile': phoneNumber}
+            q1 = session.post('https://game.energy.ch/', data)
+            tree = html.fromstring(q1.content)
+
+            frage = tree.xpath('//form[@class="question"]/h1/text()')[0]
+
+            for i in range(9):
+                antwort = get_answer(frage)
+                frage = next_question(antwort)
+            antwort = get_answer(frage)
+
+            data = {'question': antwort}
+            q2 = session.post('https://game.energy.ch/', data)
+            print("alles beantwortet")
+
+            tree = html.fromstring(q2.content)
+            verloren = tree.xpath('//div[@id="content"]/h2/text()')
+            print(verloren)
+            if verloren[0] == "Glückwunsch!":
+                data = {'site': 'win'}
+                q2 = session.post('https://game.energy.ch/', data)
+                print("auf auswahl seite")
+                q2 = session.get('https://game.energy.ch/?ticket=10')
+                print("auf endseite")
+                tree = html.fromstring(q2.content)
+                verloren = tree.xpath('//div[@id="wingame"]/h1/text()')
+                print(q2.content)
+                if len(verloren) == 1:
+                    if verloren[0] != "Das war das falsche Logo, knapp daneben! Versuche es erneut!":
+                        f = open('win.txt', 'wb')
+                        f.write(q2.content)
+                        f.close()
+                        print("gewonnen!!??")
+                    else:
+                        print("verloren - restart")
+                else:
+                    print("Etwas ist schief gelaufen - restart")
+                    schief += 1
+            else:
+                wrong += 1
+                print("zu wenig richtig - restart")
+
+        except Exception:
+            print("Es gab ein Fehler - restart")
+            errors += 1
+            pass
+finally:
+    print("\n\n--FERTIG--")
+    print("Anzahl Runden insgesamt: " + str(rounds))
+    print("Anzahl Runden falsch beantwortet: " + str(wrong))
+    print("Anzahl Fehler aufgetreten: " + str(errors))
+    print("wubb wubb debug: " + str(schief))
+    print("unanswered questions:")
+    print(*unanswered.keys(), sep='\n')
+    sys.exit(0)
